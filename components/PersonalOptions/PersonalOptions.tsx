c5ceeb31e75ee59669e79123017f6ce1fc188846
Ankan-Basu <an.basu.kan@gmail.com>
2023-03-03 14:16:23 +0530

    sidebar options component created

--- components/PersonalOptions/PersonalOptions.tsx
+++ components/PersonalOptions/PersonalOptions.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import {FaEdit, FaRegSun, FaUserAlt} from 'react-icons/fa';
 
 export default function PersonalOptions(){
   return (
-    <div className='border-2 border-solid border-red-400'>
-        <div>Add Post</div>
-        <div>See Account</div>
-        <div>Settings</div>
+    <div className='p-1 rounded-lg shadow-lg flex flex-col gap-3'>
+        <div className='p-1 flex justify-center gap-1 items-center
+        border-2 border-solid border-primary2 rounded-lg cursor-pointer bg-primary hover:bg-primary2 hover:text-white'>
+            <FaEdit /><h4>Add Post</h4>
+        </div>
+        <div className='p-2 flex gap-1 items-center rounded-lg cursor-pointer hover:bg-secondary'>
+            <FaUserAlt /><h4>My Profile</h4>
+        </div>
+        <div className='p-2 flex gap-1 items-center rounded-lg cursor-pointer hover:bg-secondary'>
+            <FaRegSun />
+            <h4>Settings</h4>
+        </div>
     </div>
   )
 }
