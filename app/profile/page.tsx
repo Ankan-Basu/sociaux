33634e28b531179c5595407f23b5bb37388f2572
Ankan-Basu <an.basu.kan@gmail.com>
2023-03-03 11:45:58 +0530

    profile coloured

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,14 +3,16 @@ import {FaPen} from 'react-icons/fa';
 
 export default function Profile(){
   return (
-    <div className='p-1 w-60 flex flex-col gap-3 border-2 border-solid border-red-300'>
+    <div className='p-1 w-60 flex flex-col gap-3'>
 
     <div className='flex items-baseline'>
-        <img src='ayaka.jpg' height='220px' width='220px' className='rounded-full border-2 border-solid border-red-400'/>
-        <div className='p-3 relative bottom-4 right-9 border-2 border-solid border-red-500 rounded-full inline-block'><FaPen /></div>
+        <img src='ayaka.jpg' height='220px' width='220px' className='rounded-full shadow-lg'/>
+        <div className='p-3 relative bottom-4 right-9 bg-secondary2 text-primary rounded-full inline-block shadow-lg'>
+            <FaPen />
+            </div>
     </div>
-    <div className='p- flex flex-col gap-1 border-2 border-solid border-green-500 rounded-lg'>
-        <div className='p-1 border-2 border-solid border-pink-500 rounded-lg'>
+    <div className='p-1 flex flex-col gap-1 rounded-lg shadow-lg'>
+        <div className='p-1 rounded-lg'>
         <div><h2 className='text-lg font-semibold'>Kamisato Ayaka</h2></div>
         <div><h2 className='font-medium'>@aether_simp</h2></div>
         </div>
@@ -19,13 +21,12 @@ export default function Profile(){
         </div>
         <div>
             <div className='mt-1 p-1 flex items-center justify-between'>
-                Bio:
+                <h3 className='font-medium'>Bio:</h3>
              <FaPen />    
             </div>
              
-        <div className='p-1 h-28 border-2 border-solid border-blue-200 rounded-lg'>
-            First I become sword fighter <br />
-            Then I do the Travell...
+        <div className='bg-secondary2 p-1 h-28 rounded-lg'>
+           
         </div>
         </div>
     </div>
@@ -36,7 +37,7 @@ export default function Profile(){
 function FriendButton() {
     return (
         <>
-        <button className='p-1 px-2 rounded-lg border-2 border-solid border-yellow-500'>
+        <button className='p-1 px-2 bg-primary rounded-lg border-2 border-solid border-primary2'>
             Add Friend
         </button>
         </>
