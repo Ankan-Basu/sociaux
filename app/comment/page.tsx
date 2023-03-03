1d1755f62048ac0ed3419803dd3fee43e80edc48
Ankan-Basu <an.basu.kan@gmail.com>
2023-03-03 11:02:22 +0530

    comment coloured

--- app/comment/page.tsx
+++ app/comment/page.tsx
@@ -5,14 +5,14 @@ import {FaEllipsisV, FaEllipsisH} from 'react-icons/fa';
 export default function Comment(){
   return (
     <>
-    <div className='flex w-98 border-2 border-solid border-red-500'>
-        <div className='w-16 border-2 border-solid border-green-500'>
-        <div className='w-full border-2 border-solid border-orange-500'>
+    <div className='flex w-98'>
+        <div className='w-16'>
+        <div className='w-full'>
             <img src='ayaka.jpg' height='60rem' width='60rem' className='rounded-full'/>
         </div>
         </div>
-        <div className='flex-1 border-2 border-solid border-blue-500'>
-            <div className='rounded-lg rounded-tl-none border-2 border-solid border-black'>
+        <div className='flex-1'>
+            <div className='bg-secondary rounded-lg rounded-tl-none'>
                 <div className='flex justify-between p-1'>
                     <div className='flex gap-1 items-center'>
                     <span><h4 className='m-0 p-0 font-medium'> Kamisato Ayaka </h4></span>
@@ -22,7 +22,7 @@ export default function Comment(){
                         <FaEllipsisH />
                     </div>
                 </div>
-                <div>
+                <div className='p-2 pt-0'>
                 Lorem ipsum dolor sit, amet consectetur 
                 adipisicing elit. Maiores animi in libero 
                 ratione sed architecto sit, repudiandae eum porro, 
@@ -30,19 +30,15 @@ export default function Comment(){
                 tempore eius illum! Totam unde deleniti eos voluptas!    
                 </div>                
             </div>
-            <div className='pl-2 text-sm'>
-                <span>Like</span>
-                <span></span>
+            <div className='px-2 text-sm flex justify-between'>
+                <div>
+                <span className='mr-2'>Like</span>
+                <span>Comment</span>
+                </div>
+                <div>20 wks</div>
             </div>
             
         </div>
-    </div>
-
-
-
-
-    <div className='w-100 border-2 border-solid border-red-500'>
-        page
     </div>
     </>
   )
