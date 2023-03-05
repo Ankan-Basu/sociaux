f7a864c880e6220b91f5e6df7e06d7efef179cbe
Ankan-Basu <an.basu.kan@gmail.com>
2023-03-05 20:06:09 +0530

    created signup api

--- Db/models/User.ts
+++ Db/models/User.ts
@@ -1,7 +1,10 @@
 import {Schema, models, model} from "mongoose";
+const bcrypt = require('bcrypt');
 
 interface IUser {
     name: string,
+    email: string,
+    uname: string,
     password: string
 }
 
@@ -10,11 +13,29 @@ const userSchema: Schema = new Schema<IUser>({
         type: String,
         required: true
     },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    uname: {
+        type: String,
+        required: true,
+        unique: true
+    },
     password: {
         type: String,
         required: true
     }
 });
+
+userSchema.pre('save', async function(next) {
+    console.log('pre save hook')
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    console.log('saving', this);
+    next();
+})
 
 var UserModel = models.user || model<IUser>('user', userSchema);
 export default UserModel;
