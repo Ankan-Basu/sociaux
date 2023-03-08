54bc82ed7e4b2e12c0763f2d2df9650153a0e510
Ankan-Basu <an.basu.kan@gmail.com>
2023-03-08 18:05:28 +0530

    post posts from ui

--- components/modal/Modal.tsx
+++ components/modal/Modal.tsx
@@ -1,10 +1,53 @@
-import React from 'react'
+import { RESPONSE_LIMIT_DEFAULT } from 'next/dist/server/api-utils';
+import React, { useState } from 'react'
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 // import { FaShare, FaLock, FaGlobe, FaEllipsisV, FaEllipsisH, FaTrash, FaRegTimesCircle, FaImage, FaVideo } from 'react-icons/fa';
 import { FiTrash, FiArrowRight, FiCornerUpRight, FiX, FiImage, FiVideo } from "react-icons/fi";
 
+
+interface PostBody {
+    uname: string;
+    privacy: number;
+    message: string;
+    shares?: number;
+}
+
+
+
 export default function Modal() {
+
+    const [postMessage, setPostMessage] = useState<string>('');
+    
+
+    const uname = 'kamisato_ayaka'; //change later
+    const privacy = 0;
+    const handlePost = async () => {
+        console.log(postMessage);
+
+        const postBody: PostBody = 
+        {uname, message: postMessage, privacy};
+
+        const url = '/api/posts';
+
+        const resp = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(postBody)
+        });
+
+        if (resp.status === 201) {
+            const data = await resp.json();
+
+            console.log(data);
+        } else {
+            //TODO
+        }
+
+    }
+
     return (
         <div 
         className='border-solid border-2 border-secondary h-screen lg:h-auto lg:w-100 p-2 pt-1 rounded-lg'
@@ -22,7 +65,10 @@ export default function Modal() {
             <label htmlFor="textarea" className='py-1'>Write post here:</label>
 
 
-                <textarea id="textarea" name="textarea" rows={4} cols={50} 
+                <textarea 
+                value={postMessage}
+                onChange={(e)=> setPostMessage(e.target.value)}
+                id="textarea" name="textarea" rows={4} cols={50} 
                 className='bg-secondary2 w-full lg:w-99 rounded-lg outline-none p-1 resize-none'
                 >
                 </textarea>
@@ -34,12 +80,15 @@ export default function Modal() {
                     <FiVideo /></button>
             </div>
             <div className='flex justify-center flex-col gap-1 pt-1'>
-                <span className='flex-1'>
+                <span 
+                className='flex-1'>
                 <Button>
                     <FiTrash />Discard
                 </Button>
                 </span>
-                <span className='flex-1'>
+                <span 
+                onClick={handlePost}
+                className='flex-1'>
                 <Button type='normal'>
                     <FiCornerUpRight />Post</Button>
                 </span>
