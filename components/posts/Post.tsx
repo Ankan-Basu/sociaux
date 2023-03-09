5647cffcc3a038096a1036b9fdf109c09eb1b2d9
Ankan-Basu <an.basu.kan@gmail.com>
2023-03-09 19:41:41 +0530

    like/unlike post from ui

--- components/posts/Post.tsx
+++ components/posts/Post.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { FiEdit3, FiTrash, FiX, FiThumbsUp, FiCornerUpRight, FiMessageSquare, FiSettings, FiLogOut, FiUser } from "react-icons/fi";
 // const expanded = true; //will come from props
@@ -28,8 +28,82 @@ export default function Post(
     }: IPostProps
 
 ) {
+
+    const [liked, setLiked] = useState<boolean>(false);
+
+    const reactorUname = 'hu_tao' //change later
+    
+    useEffect(() => {
+        if (likes?.includes(reactorUname)) {
+            setLiked(true);
+        }
+    }, [])
+
+    const handleLike = async () => {
+        // console.log(_id);
+
+        const reqBody = {
+            "uname": reactorUname,
+            "postId": _id
+        }
+
+        const url = '/api/like/post';
+
+        const resp = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(reqBody)
+        });
+
+        if (resp.status === 201) {
+            setLiked(true);
+        }
+        const data = await resp.json();
+
+        console.log(data);
+
+    }
+
+    const handleUnlike = async () => {
+        // console.log(_id);
+
+        const reqBody = {
+            "uname": reactorUname,
+            "postId": _id
+        }
+
+        const url = '/api/like/post';
+
+        const resp = await fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(reqBody)
+        });
+
+        if (resp.status === 200) {
+            setLiked(false);
+        }
+        const data = await resp.json();
+
+        console.log(data);
+    }
+
+
+    const toggleLike = () => {
+        if (liked) {
+            handleUnlike();
+        } else {
+            handleLike();
+        }
+    }
+
+
   return (
-    <Link href={`/post/${_id}`}>
+    // <Link href={`/post/${_id}`}>
     <div 
     className={`
     ${expanded?'w-full':''}
@@ -81,11 +155,18 @@ export default function Post(
 Optional Img/ Vid
             </div>
             <div className='text-primary flex justify-center gap-2 pt-1'>
-               <span className='py-1 flex-1 flex justify-center items-center gap-1 border-solid border-2 border-primary rounded-lg'>
+               <span 
+               onClick={toggleLike}
+               className={`
+               cursor-pointer
+               py-1 flex-1 flex justify-center items-center gap-1 
+               ${liked?'bg-primary text-white':'bg-white text-primary'}
+               border-solid border-2 border-primary rounded-lg`}>
                 <FiThumbsUp /> 
                 
                 {/* Display Text only in big screen */}
-                <span className='hidden lg:inline'>
+                <span 
+                className='hidden lg:inline'>
                     Like
                     {likes?.length}
                     </span>
@@ -109,7 +190,7 @@ Optional Img/ Vid
                 
             </div>
         </div>
-        </Link>
+        // </Link>
   )
 }
 
