391d9a1cd6f4b823982a6f9f31dc892ebffd2644
Ankan-Basu <an.basu.kan@gmail.com>
2023-03-09 17:29:48 +0530

    created reply to comment api routes

--- Db/models/ReplyComment.ts
+++ Db/models/ReplyComment.ts
@@ -1,9 +1,38 @@
-import mongoose from "mongoose";
+import { Schema, model, models } from "mongoose";
 
 export interface IReplyComment {
     uname: string;
     parenCommId: string;
     message: string;
-    likes: Array<string>;
-    time: string;
+    likes?: Array<string>;
+    time?: Date;
 }
+
+const replyCommentSchema: Schema = new Schema<IReplyComment>({
+    uname: {
+        type: String,
+        required: true
+    },
+    parenCommId: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    likes: {
+        type: [String],
+        required: true,
+        default: []
+    },
+    time: {
+        type: Date,
+        required: true,
+        default: Date.now()
+    }
+});
+
+const ReplyCommentModel = models.replyComment || model<IReplyComment>('replyComment', replyCommentSchema);
+
+export default ReplyCommentModel;
---
+++ pages/api/comments/reply/[commentId].ts
@@ -0,0 +1,132 @@
+import dbConnect from "@/Db/middleware/mongodb";
+import CommentModel, { IComment } from "@/Db/models/Comment";
+import ReplyCommentModel, { IReplyComment } from "@/Db/models/ReplyComment";
+import { HydratedDocument } from "mongoose";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        dbConnect();
+
+
+        //add reply
+        if (req.method === 'POST') {
+            const reqBody: IPostReqBody = req.body;
+
+            try {
+                const parenComm: HydratedDocument<IComment> | null = await CommentModel.findOne({_id: reqBody.parenCommId});
+
+                if (!parenComm) {
+                    // ie null
+                    res.status(404).json('Not Found');
+                } else {
+                    const replyCommObj: IReplyComment = {
+                        uname: reqBody.uname,
+                        parenCommId: reqBody.parenCommId,
+                        message: reqBody.message
+                    };
+
+                    try {
+                        const replyComm: HydratedDocument<IReplyComment> | null = await ReplyCommentModel.create(replyCommObj);
+
+                        if (!replyComm) {
+                            res.status(500).json('Err saving comment');    
+                        } else {
+                            parenComm.replies.push(replyComm._id.toString());
+                            
+                            const dbResp = await parenComm.save();
+
+                            res.status(201).json(dbResp);
+                        }
+
+                    } catch(err) {
+                        res.status(500).json('Err saving comment');
+                    }
+                }
+            } catch (err) {
+                /**
+                 * if user provided parenCommId which cannot be type casted into Object Id
+                 */
+                res.status(404).json('Not Found');
+            }
+        }
+
+
+        //delete reply
+        else if (req.method === 'DELETE') {
+            const reqBody: IDeleteReqBody = req.body;
+
+            try {
+                const parenComm: HydratedDocument<IComment> | null = await CommentModel.findOne({_id: reqBody.parenCommId});
+
+                if (!parenComm) {
+                    // ie null
+                    res.status(404).json('Not Found');
+                } else {
+                    try {
+                       // remove from parenComm
+
+                    //    if (parenComm.replies.includes(reqBody.commentId)) {
+
+                    //    }
+                    //    const repliesArr = parenComm.replies.filter((reply) => {
+                    //         return reply !== reqBody.commentId;
+                    //    });
+                        
+                    // try in single pass
+                        const repliesArr: Array<string> = [];
+
+                        const len = parenComm.replies.length;
+                        let found = false;
+
+                        for (let i=0; i<len; i++) {
+                            if (parenComm.replies[i] === reqBody.commentId) {
+                                found = true;
+                            } else {
+                                repliesArr.push(parenComm.replies[i]);
+                            }
+                        }
+                        /*********************************************** */
+                       
+                        if (!found) {
+                            res.status(404).json('Reply comment doesnt exist');
+                        } else {
+                            parenComm.replies = repliesArr;
+
+                            const dbResp1 = await parenComm.save();
+                            // err will be catched by catch block;
+
+                            const dbResp = await ReplyCommentModel.deleteOne({_id: reqBody.commentId});
+
+                            res.status(200).json(dbResp);
+                        }
+                       
+                    } catch(err) {
+                        res.status(500).json('Err saving comment');
+                    }
+                }
+            } catch (err) {
+                /**
+                 * if user provided parenCommId which cannot be type casted into Object Id
+                 */
+                res.status(404).json('Not Found');
+            }
+        }
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json('err');
+    }
+}
+
+interface IPostReqBody {
+    uname: string;
+    parenCommId: string;
+    message: string;
+}
+
+interface IDeleteReqBody {
+    uname: string;
+    parenCommId: string;
+    commentId: string;
+}
