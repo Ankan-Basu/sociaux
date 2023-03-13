f67ee204babf4a2ccc032991338dbb21e9095ece
Ankan-Basu <an.basu.kan@gmail.com>
2023-03-13 23:25:57 +0530

    Setting up prisma(2)

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,6 +1,7 @@
 import dbConnect from "@/Db/middleware/mongodb";
 import UserModel from "@/Db/models/User"
 import { NextApiRequest, NextApiResponse } from "next"
+import { prisma } from "@/utils/prisma";
 
 
 export default async function handler (req: NextApiRequest, res: NextApiResponse) {
@@ -8,8 +9,18 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
 
   await dbConnect();
   if(method === 'GET') {
-    const user = await UserModel.findOne({
-        name: 'A b'
+    await prisma.$connect();
+    // const user = await UserModel.findOne({
+    //     name: 'A b'
+    // });
+
+    const user = await prisma.user.create({
+      data: {
+        email: 'a2@b.c',
+        name: 'A B',
+        uname: 'ab2',
+        password: 'abcd',
+      }
     });
 
     res.status(200).json(user);
