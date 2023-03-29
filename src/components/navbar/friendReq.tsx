a646335f90bdf441922c23325c973ff3e40e077d
Ankan Basu <an.basu.kan@gmail.com>
2023-03-29 12:35:41 +0530

    Accept / Reject frend req from ui now

--- src/components/navbar/friendReq.tsx
+++ src/components/navbar/friendReq.tsx
@@ -1,4 +1,6 @@
+import { useSession } from "next-auth/react"
 import { FC } from "react"
+import { api } from "~/utils/api"
 
 interface IFrenReqProps {
     friendReq: {
@@ -7,6 +9,45 @@ interface IFrenReqProps {
 }
 
 const FrenReq: FC<IFrenReqProps> = ({friendReq}) => {
+
+    const session = useSession();
+
+    const acceptFriendMutation = api.friends.acceptFriendReq.useMutation();
+    const rejectFriendMutation = api.friends.rejectFriendReq.useMutation();
+
+    const handleAccept = async (targetUname: string) => {
+        if (session.status !== 'authenticated'){
+            console.log('UnauThen');
+            return;
+        }
+
+        const acceptorUname = session.data.user.uname;
+        if (!acceptorUname || !targetUname) {
+            console.log('BAD REQ');
+            return;
+        }
+
+        const resp = await acceptFriendMutation.mutateAsync({acceptorUname, targetUname});
+        console.log(resp);
+        
+    }
+
+    const handleReject = async (targetUname: string) => {
+        if (session.status !== 'authenticated'){
+            console.log('UnauThen');
+            return;
+        }
+
+        const rejectorUname = session.data.user.uname;
+        if (!rejectorUname || !targetUname) {
+            console.log('BAD REQ');
+            return;
+        }
+
+        const resp = await rejectFriendMutation.mutateAsync({rejectorUname, targetUname});
+        console.log(resp);
+        
+    }
     return (
         <div className='mb-4 shadow-lg rounded-lg p-1 flex gap-1'>
             <img src='ayaka.jpg' className='w-12 h-12 rounded-full' />
@@ -22,9 +63,13 @@ const FrenReq: FC<IFrenReqProps> = ({friendReq}) => {
                     
                     </div>
                     <div className='flex justify-between pl-1 pr-2'>
-                <span className='text-xs font-medium cursor-pointer hover:text-primary active:text-primary2'>
+                <span
+                onClick={() => handleAccept(friendReq.source)} 
+                className='text-xs font-medium cursor-pointer hover:text-primary active:text-primary2'>
                     Accept</span>
-                <span className='text-xs font-medium cursor-pointer hover:text-primary active:text-primary2'>
+                <span 
+                onClick={() => handleReject(friendReq.source)}
+                className='text-xs font-medium cursor-pointer hover:text-primary active:text-primary2'>
                     Reject</span>
                     </div>
             </div>
