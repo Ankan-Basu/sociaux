4495d9899f76c7e27a1a17732a528333e4408987
Ankan Basu <an.basu.kan@gmail.com>
2023-03-29 23:01:57 +0530

    Adding functionality to the friend buttons

--- src/components/profile/button.tsx
+++ src/components/profile/button.tsx
@@ -1,3 +1,5 @@
+import { useSession } from 'next-auth/react';
+import { bracketSameLine } from 'prettier.config.cjs';
 import React, { FC, useEffect, useState } from 'react'
 import { string } from 'zod'
 import { api } from '~/utils/api';
@@ -10,6 +12,13 @@ const ButtonTest: FC<IProps> = ({profileUname}) => {
   const [buttonState, setButtonState] = useState<Number>(0);
   const buttonQuery = api.button.getState.useQuery({profileUname});
 
+  const sendFriendReqMutation = api.friends.sendFriendReq.useMutation();
+  const unFriendMutation = api.friends.unFriend.useMutation();
+  const cancelFriendReqMutation = api.friends.cancelFriendReq.useMutation();
+  const acceptFriendReqMutation = api.friends.acceptFriendReq.useMutation();
+
+  const session = useSession();
+
   useEffect(() => {
     if (buttonQuery.isFetched) {
       if (buttonQuery.data?.code) {
@@ -20,11 +29,43 @@ const ButtonTest: FC<IProps> = ({profileUname}) => {
     }
   }, [buttonQuery])
   
-  useEffect(() => {
-    console.log('BUTTON MOUNTS');
-    
-  }, [])
-  console.log('Button Test, query', buttonQuery);
+
+  const handleButtonClick = async (mode: string) => {
+    if (session.status !== 'authenticated') {
+      console.log('Unauthenticated');
+      return;
+    }
+
+    const requesterUname = session.data?.user.uname;
+    if (!requesterUname) {
+      console.log('Unauth');
+      return;
+    }
+
+    let resp = undefined;
+    switch(mode) {
+      case 'sendReq':
+        resp = await sendFriendReqMutation.mutateAsync({requesterUname, targetUname: profileUname});
+        break;
+
+      case 'acceptReq':
+        resp = await acceptFriendReqMutation.mutateAsync({acceptorUname: requesterUname, targetUname: profileUname});
+        break;
+
+      case 'cancelReq':
+        resp = await cancelFriendReqMutation.mutateAsync({cancellerUname: requesterUname, targetUname: profileUname});
+        break;
+
+      case 'unFriend':
+        resp = await unFriendMutation.mutateAsync({unFrienderUname: requesterUname, targetUname: profileUname});
+        break;
+
+      default:
+        console.log('ERROR switch case');
+        break;
+
+    }
+  }
 
 // if(buttonQuery.isFetching) {
 //   return (
@@ -53,6 +94,7 @@ const ButtonTest: FC<IProps> = ({profileUname}) => {
   else if(buttonState === 1) {
     return (
       <button 
+      onClick={() => handleButtonClick('sendReq')}
     className="rounded-lg border-2 border-solid border-primary2 bg-primary p-1 lg:px-2"
     >
       <span className="text-sm lg:text-base">Add Friend</span>
@@ -63,6 +105,7 @@ const ButtonTest: FC<IProps> = ({profileUname}) => {
   else if(buttonState === 2) {
     return (
       <button 
+      onClick={() => handleButtonClick('acceptReq')}
     className="rounded-lg border-2 border-solid border-primary2 bg-primary p-1 lg:px-2"
     >
       <span className="text-sm lg:text-base">Accept Request</span>
@@ -74,6 +117,7 @@ const ButtonTest: FC<IProps> = ({profileUname}) => {
     return (
 
       <button 
+      onClick={() => handleButtonClick('cancelReq')}
       className="rounded-lg border-2 border-solid border-primary2 bg-primary p-1 lg:px-2"
       >
       <span className="text-sm lg:text-base">Cancel Request</span>
@@ -85,6 +129,7 @@ const ButtonTest: FC<IProps> = ({profileUname}) => {
     return (
 
       <button 
+      onClick={() => handleButtonClick('unFriend')}
       className="rounded-lg border-2 border-solid border-primary2 bg-primary p-1 lg:px-2"
       >
       <span className="text-sm lg:text-base">Unfriend</span>
