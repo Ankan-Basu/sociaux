52f2e48852b3346f7f7ff2caaea3520552ce7dc1
Ankan Basu <an.basu.kan@gmail.com>
2023-03-29 21:55:34 +0530

    Bug fix (showing loading for long time) in friend button

--- src/components/profile/button.tsx
+++ src/components/profile/button.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { string } from 'zod'
 import { api } from '~/utils/api';
 
@@ -7,33 +7,50 @@ interface IProps {
 }
 
 const ButtonTest: FC<IProps> = ({profileUname}) => {
+  const [buttonState, setButtonState] = useState<Number>(0);
   const buttonQuery = api.button.getState.useQuery({profileUname});
 
-  // useEffect(() => {
-
-  // }, [])
+  useEffect(() => {
+    if (buttonQuery.isFetched) {
+      if (buttonQuery.data?.code) {
+        console.log('SET BUTTON STATE', buttonState);
+        
+        setButtonState(buttonQuery.data?.code)
+      }
+    }
+  }, [buttonQuery])
   
+  useEffect(() => {
+    console.log('BUTTON MOUNTS');
+    
+  }, [])
   console.log('Button Test, query', buttonQuery);
 
-if(buttonQuery.isFetching) {
-  return (
-    <button 
-    className="rounded-lg border-2 border-solid border-primary2 bg-primary p-1 lg:px-2"
-    >
-      <span className="text-sm lg:text-base">Loading ...</span>
-    </button>
-  )
-}
-  else if (buttonQuery.isFetched) {
+// if(buttonQuery.isFetching) {
+//   return (
+//     <button 
+//     className="rounded-lg border-2 border-solid border-deactiv bg-deactiv p-1 lg:px-2"
+//     >
+//       <span className="text-sm lg:text-base">Loading ...</span>
+//     </button>
+//   )
+// }
+  // else if (buttonQuery.isFetched) {
 
     // return (
     //   <>{JSON.stringify(buttonQuery.data)}</>
     // )
-  if (buttonQuery.data?.code === 0) {
-    return <>code 0</>
+  if (buttonState === 0) {
+    return (
+      <button 
+    className="rounded-lg border-2 border-solid border-deactiv bg-deactiv p-1 lg:px-2"
+    >
+      <span className="text-sm lg:text-base">Loading ...</span>
+    </button>
+    )
   }
 
-  else if(buttonQuery.data?.code === 1) {
+  else if(buttonState === 1) {
     return (
       <button 
     className="rounded-lg border-2 border-solid border-primary2 bg-primary p-1 lg:px-2"
@@ -43,7 +60,7 @@ if(buttonQuery.isFetching) {
     )
   }
 
-  else if(buttonQuery.data?.code === 2) {
+  else if(buttonState === 2) {
     return (
       <button 
     className="rounded-lg border-2 border-solid border-primary2 bg-primary p-1 lg:px-2"
@@ -53,7 +70,7 @@ if(buttonQuery.isFetching) {
     )
   }
 
-  else if(buttonQuery.data?.code === 3) {
+  else if(buttonState === 3) {
     return (
 
       <button 
@@ -64,7 +81,7 @@ if(buttonQuery.isFetching) {
       );
   }
 
-  else if(buttonQuery.data?.code === 4) {
+  else if(buttonState === 4) {
     return (
 
       <button 
@@ -83,7 +100,7 @@ if(buttonQuery.isFetching) {
       </>
     )
   }
-}
+// }
 
 
   
