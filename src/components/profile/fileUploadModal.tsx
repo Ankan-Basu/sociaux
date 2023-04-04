40093f8916d76ea3d7124a6191bd1608c037e96c
Ankan-Basu <an.basu.kan@gmail.com>
2023-04-04 22:00:57 +0530

    CSS and error handling fix to profile img upload

--- src/components/profile/fileUploadModal.tsx
+++ src/components/profile/fileUploadModal.tsx
@@ -1,7 +1,11 @@
+import { TRPCClientError } from "@trpc/client";
 import { router } from "@trpc/server";
 import { useSession } from "next-auth/react";
+import Image from "next/image";
 import { useRouter } from "next/router";
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, useContext, useEffect, useRef, useState } from "react";
+import { FiImage, FiX } from "react-icons/fi";
+import { ErrorContext } from "~/contexts/errorContext";
 import { api } from "~/utils/api";
 
 interface IFileUploadModalProps {
@@ -18,6 +22,10 @@ const FileUploadModal: FC<IFileUploadModalProps> = ({display, setDisplay, uname,
 
   const profileImgMutation = api.users.uploadProfileImage.useMutation();
 
+  const {setErrorDisplay, setErrorMessage, setErrorType} = useContext(ErrorContext);
+
+  const formRef = useRef<HTMLInputElement>(null);
+
   const router = useRouter(); 
 
 
@@ -30,6 +38,9 @@ const FileUploadModal: FC<IFileUploadModalProps> = ({display, setDisplay, uname,
     const reader = new FileReader();
 
     if (!file) {
+      setErrorDisplay(true);
+      setErrorMessage('No image chosen');
+      setErrorType('simple');
       return;
     }
     reader.readAsDataURL(file);
@@ -37,6 +48,9 @@ const FileUploadModal: FC<IFileUploadModalProps> = ({display, setDisplay, uname,
     reader.onload = () => {
       // console.log(reader.result);
       if (!reader.result) {
+        setErrorDisplay(true);
+      setErrorMessage('An unexpected error occured');
+      setErrorType('simple');
         return;
       }
       const imgStr = reader.result as string
@@ -44,24 +58,36 @@ const FileUploadModal: FC<IFileUploadModalProps> = ({display, setDisplay, uname,
   
     }
     reader.onerror = (err) => {
-      console.log(err);
+      // console.log(err);
+      setErrorDisplay(true);
+      setErrorMessage('An unexpected error occured');
+      setErrorType('simple');
     }
 }
 
 const handleProfilImgUpload = async (e: React.FormEvent<HTMLFormElement>) => {
   e.preventDefault();
   if (!img) {
-    console.log('No image selected');
+    // console.log('No image selected');
+    setErrorDisplay(true);
+      setErrorMessage('No image chosen');
+      setErrorType('simple');
     return;
   }
 
   if (session.status !== 'authenticated') {
-    console.log('UNAUTHORISED');
+    // console.log('UNAUTHORISED');
+    setErrorDisplay(true);
+      setErrorMessage('You need to login to upload profile picture');
+      setErrorType('logout');
     return;
   }
 
   if (uname !== session.data.user.uname) {
-    console.log('Dont edit others pic');
+    // console.log('Dont edit others pic');
+    setErrorDisplay(true);
+      setErrorMessage('UNAUTHORISED');
+      setErrorType('logout');
     return;
   }
 
@@ -69,16 +95,21 @@ const handleProfilImgUpload = async (e: React.FormEvent<HTMLFormElement>) => {
 
     const x = await profileImgMutation.mutateAsync({uname: session.data?.user.uname, image: img});
     setDisplay(false);
-    const newImg = x.dbResp?.img;
-    // console.log('NEW IMG', newImg);
     
-    // setProfileImg(newImg)
+    
     setProfileImg(img);
     // console.log(newImg)
     // router.push(`/user/${session.data.user.uname}`);
     // router.push('/dashboard');
   } catch(err) {
-    console.log(err)
+    // console.log(err)
+    setErrorDisplay(true);
+    let msg = 'An unknown error occured';
+    if (err instanceof TRPCClientError) {
+      msg = err.data.code;
+    }
+      setErrorMessage(msg);
+      setErrorType('simple');
   }
 
 }
@@ -105,12 +136,35 @@ const handleProfilImgUpload = async (e: React.FormEvent<HTMLFormElement>) => {
         {/* <div>
           <FiX />
         </div> */}
-        <div className="flex flex-col justify-center items-center gap-3">
+        <div className="flex flex-col justify-center items-center gap-3 p-2">
+          <div className={`
+          ${img?'block':'hidden'}
+          border-2 border-solid border-red-500 relative`}>
+            <span 
+            onClick={() => {
+              setImg('');
+              if (!formRef.current) {
+                return;
+              }
+              formRef.current.value = '';
+            }}
+            className="absolute bg-secondary2 text-primary p-2 rounded-full -top-4 -right-4" >
+            <FiX />
+            </span>
+            <Image src={img || ''} height={100} width={100} alt={'image'} />
+            </div>
         <form 
         onSubmit={handleProfilImgUpload}
         className="flex flex-col justify-center items-center gap-3"
         >
-          <input type={'file'} accept='image/*' onChange={handleImg} />
+          <input type={'file'} accept='image/*' onChange={handleImg} ref={formRef} className='invisible' />
+          <button 
+          onClick={() => {
+            formRef.current?.click();
+          }}
+          type='button'
+          className="p-2 rounded-lg bg-deactiv flex justify-center items-center gap-1"
+          ><FiImage />Choose Image</button>
           <button type="submit"
           className="
           p-2 rounded-lg 
