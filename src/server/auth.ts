d4b0135da7f3f4af056cbb806c2f0c4c94b01a9b
Ankan Basu <60862627+Ankan-Basu@users.noreply.github.com>
2025-04-07 13:20:43 +0530

    merge from release branch
    * Cursor fix
    
    fixes cursor type (to pointer) for hovering over "Comment" and "Share" button
    
    * fixing ts errors
    
    * Update browserslist
    
    ran "npx update-browserslist-db"
    
    * resolving errors

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -18,6 +18,7 @@ declare module "next-auth" {
   interface Session extends DefaultSession {
     user: {
       id: string;
+      uname: string;
       // ...other properties
       // role: UserRole;
     } & DefaultSession["user"];
